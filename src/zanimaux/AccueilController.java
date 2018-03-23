969d5f5eb49ee0e66540b5fd2a10eb2cac250e5e
MarouaArifa <maroua.arifa@esprit.tn>
2018-03-23 20:59:44 +0100

    tsawer

---
+++ src/zanimaux/AccueilController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zanimaux;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Maroua
+ */
+public class AccueilController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorP;
+    @FXML
+    private Button userName;
+    @FXML
+    private Pane pane;
+    @FXML
+    private Button btn11;
+    @FXML
+    private Button btn1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void showPane(MouseEvent event) {
+    }
+
+    @FXML
+    private void connexionAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void hidePane(MouseEvent event) {
+    }
+    
+}

af2c8eeaa2f9245ff067f89d8b42ebaec6feac72
Azza <azza.maalej@esprit.tn>
2018-03-24 01:30:06 +0200

    userService avec bcrypte

--- src/zanimaux/entities/User.java
+++ src/zanimaux/entities/User.java
@@ -3,63 +3,116 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package zanimaux.entities;
 
 import java.util.Objects;
 
 /**
  *
- * @author Mariam
+ * @author Azza
  */
 public class User {
-    private  String nom;
-    private String prenom;
-    private  int cin;
+    private String cin;
+    private String username ;
+    private String username_canonical ;
+    private String email ;
+    private String email_canonical ;
+    private int enabled ;
+    private String password;
+    private String nom ;
+    private String prenom ;
+    private int telephone;
+    private String adresse;
+    private String ville;
+    private int codePostale;
+    private String roles;
 
-    public User() {
+    public String getRoles() {
+        return roles;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 97 * hash + this.cin;
-        return hash;
+    public void setRoles(String roles) {
+        this.roles = roles;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final User other = (User) obj;
-        if (!Objects.equals(this.nom, other.nom)) {
-            return false;
-        }
-        if (!Objects.equals(this.prenom, other.prenom)) {
-            return false;
-        }
-        if (this.cin != other.cin) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" + "nom=" + nom + ", prenom=" + prenom + ", cin=" + cin + '}';
-    }
-
-    public User(String nom, String prenom, int cin) {
+    public User(String cin, String username, String email, String password, String roles, String nom, String prenom, int telephone, String adresse, String ville, int codePostale) {
+        this.cin = cin;
+        this.username = username;
+        this.username_canonical = username;
+        this.email = email;
+        this.email_canonical = email;
+        this.enabled = 1;
         this.nom = nom;
         this.prenom = prenom;
+        this.telephone = telephone;
+        this.adresse = adresse;
+        this.ville = ville;
+        this.codePostale = codePostale;
+        if (roles=="PROPRIETAIRE_REFUGE")
+        {this.roles= "a:1:{i:0;s:24:\"ROLE_"+roles+"\";}";}
+        this.password= password;
+    }
+   
+  
+    //String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
+
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
         this.cin = cin;
     }
 
-    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername_canonical() {
+        return username_canonical;
+    }
+
+    public void setUsername_canonical(String username_canonical) {
+        this.username_canonical = username_canonical;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail_canonical() {
+        return email_canonical;
+    }
+
+    public void setEmail_canonical(String email_canonical) {
+        this.email_canonical = email_canonical;
+    }
+
+    public int getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(int enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getNom() {
         return nom;
     }
@@ -76,14 +129,69 @@ public class User {
         this.prenom = prenom;
     }
 
-    public int getCin() {
-        return cin;
+    public int getTelephone() {
+        return telephone;
     }
 
-    public void setCin(int cin) {
-        this.cin = cin;
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
     }
 
-  
-   
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + Objects.hashCode(this.cin);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "cin=" + cin + ", username=" + username + ", email=" + email + ", nom=" + nom + ", prenom=" + prenom + ", telephone=" + telephone + ", adresse=" + adresse + ", ville=" + ville + ", codePostale=" + codePostale + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.cin, other.cin)) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public int getCodePostale() {
+        return codePostale;
+    }
+
+    public void setCodePostale(int codePostale) {
+        this.codePostale = codePostale;
+    }
+    
+
+    
+    
 }

e37087de59f9beb5a602e202ceb48d98e4716bae
oumaimaSouabni <oumaima.souabni@esprit.tn>
2018-03-28 23:21:07 +0100

    OUMAIMA

--- src/zanimaux/Service/PanierService.java
+++ src/zanimaux/Service/PanierService.java
@@ -13,6 +13,7 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import zanimaux.Technique.DataSource;
+import zanimaux.entities.ContenuPanier;
 import zanimaux.entities.Magasin;
 import zanimaux.entities.Panier;
 import zanimaux.entities.Produit;
@@ -23,7 +24,7 @@ import zanimaux.entities.User;
  * @author macbookpro
  */
 public class PanierService {
-        public Connection con = DataSource.getInstance().getCon();
+    public Connection con = DataSource.getInstance().getCon();
     public Statement ste;
     
     public PanierService() throws SQLException 
@@ -44,6 +45,21 @@ public class PanierService {
             return false;
     
     }
+    public boolean modifContenuPanier(String cin, ContenuPanier cp)
+    {
+        String requete="UPDATE ContenuPanier SET quantite=?, commande=?, dateCommande=? where `cin`="+cin;
+        try {
+            PreparedStatement pst =con.prepareStatement(requete);
+            pst.setInt(1,cp.getQuantite());
+            pst.setInt(2,cp.getCommande());
+            pst.setDate(2,cp.getDateCommande());            
+            pst.executeUpdate();
+            System.out.println("modification reussite");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+            return false;
+    }
     public Panier recherchePanier(String cin)
     {
         Panier listForm = new Panier();
@@ -60,6 +76,25 @@ public class PanierService {
             Logger.getLogger(Articleservice.class.getName()).log(Level.SEVERE, null, ex);
         }
         return listForm;
+    }
+    public ContenuPanier rechercheProduitContenuPanier(String cin, Produit p)
+    {
+                ContenuPanier listForm = new ContenuPanier();
+        try {  
+            String requete = "SELECT * FROM ContenuPanier WHERE `cin`="+cin+"AND `idProduit`="+p.getIdProduit()+"`commande`=0";
+            ResultSet rs = ste.executeQuery(requete);
+             while(rs.next()){
+                 listForm.setCin(cin);
+                 listForm.setCommande(rs.getInt("commande"));
+                 listForm.setIdContenuPanier(rs.getInt("idContenuPanier"));
+                 listForm.setQuantite(rs.getInt("quntite"));
+            }
+             
+        } catch (SQLException ex) {
+            Logger.getLogger(Articleservice.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listForm;
+        
     }
     public void ajouterProduitPanier(Produit p)
     {
@@ -81,7 +116,32 @@ public class PanierService {
         else
         {
             panier.setSomme(panier.getSomme()+p.getPrix());
+            this.modifPanier("09625522", panier);
         }
+        ContenuPanier cp= this.rechercheProduitContenuPanier("0962552", p);
+        if(cp==null)
+        {
+            
+            String requete = "INSERT INTO `ContenuPanier` (`cin`, `quantite`, `idProduit`, `commande`) VALUES (?,?,?,?)";
+       
+       try {
+            PreparedStatement pst =con.prepareStatement(requete);
+            pst.setString(1,"09625522");
+            pst.setInt(2,1);
+            pst.setInt(3, p.getIdProduit());
+            pst.setInt(4, 0);
+            pst.executeUpdate();
+            System.out.println("ajout reussit");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        }
+        else
+        {
+            cp.setQuantite(cp.getQuantite()+1);
+            this.modifContenuPanier("09625522", cp);
+        }
+        
         
     }
     

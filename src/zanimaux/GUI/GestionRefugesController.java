8bcca45002e422efe62d0e6b3b02a2eecaa55061
Azza <azza.maalej@esprit.tn>
2018-03-29 00:44:07 +0200

    Azza dashboard

--- src/zanimaux/GUI/GestionRefugesController.java
+++ src/zanimaux/GUI/GestionRefugesController.java
@@ -3,10 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package zanimaux.GUI;
 
-import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,14 +13,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
@@ -33,26 +26,22 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import zanimaux.Service.AnimalService;
-
 import zanimaux.Service.RefugeService;
-import zanimaux.entities.Animal;
-
 import zanimaux.entities.Refuge;
-
+import zanimaux.entities.User;
+import zanimaux.util.Session;
 
 /**
  * FXML Controller class
  *
  * @author Azza
  */
-public class RefugeClientController implements Initializable {
+public class GestionRefugesController implements Initializable {
 
     @FXML
-    private Button buttonRF;
+    private Button GestRef;
     @FXML
-    private Button evenement;
+    private Button GestAnim;
     @FXML
     private Button userName;
     @FXML
@@ -63,7 +52,7 @@ public class RefugeClientController implements Initializable {
     private Button btn1;
     @FXML
     private AnchorPane anchorEvent;
-   
+
     /**
      * Initializes the controller class.
      */
@@ -71,13 +60,16 @@ public class RefugeClientController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
          RefugeService m = null;
         try {
+            
             m = new RefugeService();
         } catch (SQLException ex) {
             Logger.getLogger(RefugeClientController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        ResultSet r =m.AfficherTousRefuge();
+        User user=Session.getLoggedInUser();
+        String cin=user.getCin();
+        ResultSet r ;
         Refuge m1=new Refuge();
-        r= m.AfficherTousRefuge();
+        r= m.AfficherRefugeByCin(cin);
         
          ScrollPane sp = new ScrollPane();
     
@@ -88,11 +80,13 @@ public class RefugeClientController implements Initializable {
         HBox hb =null;
         vb.setPadding(new Insets(100, 30, 0, 30));
         vb.setSpacing(100);
+        
         int i=0;
         try {
             while(r.next()){
                 i++;
                 Label lbl = new Label();
+                
                 m1.setImmatriculation(r.getString("immatriculation"));
                 m1.setCin(r.getString("cin"));
                 m1.setNomRefuge(r.getString("nomRefuge"));
@@ -115,13 +109,13 @@ public class RefugeClientController implements Initializable {
                 Text t =new Text(m1.getAdresseRefuge()+" "+m1.getGouvernementRefuge()+", "+m1.getCodePostaleRefuge());
                 t.setFont(Font.font("Verdana", 15) );
                 Button b = new Button();
-                b.setText("consulter refuge");
+                b.setText("Effacer refuge");
                 b.setId(String.valueOf(m1.getImmatriculation()));
                 b.setOnAction(e->{
                     try {
-                        consulterRefuge(e);
+                        EffacerRefuge(e);
                     } catch (SQLException ex) {
-                        Logger.getLogger(accueilOumaimaController.class.getName()).log(Level.SEVERE, null, ex);
+                        Logger.getLogger(GestionRefugesController.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 });
                 VBox vbMagasin = new VBox();
@@ -156,75 +150,7 @@ public class RefugeClientController implements Initializable {
          anchorEvent.getChildren().setAll(sp);
         
     }    
-
-    void consulterRefuge(ActionEvent e) throws SQLException {
-    
-        ResultSet rs =null;
-        
-           AnimalService m= new AnimalService();
-          
-            String a =((Node)e.getSource()).getId();
-            rs=  m.RechercherAnimalByImm(a) ;
-            int i=0;
-            Animal listForm=new Animal();
-            ScrollPane sp = new ScrollPane();
-            sp.setPrefSize(500, 500);
-            sp.setMaxSize(Control.USE_COMPUTED_SIZE, Control.USE_COMPUTED_SIZE);
-            sp.setMinSize(Control.USE_COMPUTED_SIZE, Control.USE_COMPUTED_SIZE);
-            VBox vb = new VBox();
-            HBox hb =null;
-            vb.setPadding(new Insets(100, 30, 0, 30));
-            vb.setSpacing(100);
-            
-             while(rs.next())
-            {
-                listForm.setRefuge(rs.getString("refuge"));
-                 listForm.setIdAnimal(rs.getInt("idAnimal"));
-                 listForm.setType(rs.getString("type"));
-                 listForm.setEtat(rs.getString("etat"));
-                 listForm.setNomAnimal(rs.getString("nomAnimal"));
-                 listForm.setPhotoAnimal(rs.getString("photoAnimal"));
-                 listForm.setAge(rs.getInt("age"));               
-                 listForm.setRace(rs.getString("race"));
-                ImageView im= new ImageView();
-                Image image= new Image("zanimaux/ImageUtile/"+listForm.getPhotoAnimal(),150,120,false,false) ;
-                im.setImage(image);
-                Text t1 =new Text(listForm.getRace());
-                t1.setFont(Font.font("Verdana", 20));
-                Text t =new Text(listForm.getType());
-                t.setFont(Font.font("Verdana", 15) );
-                
-          
-                      VBox vbProduit = new VBox(); 
-          vbProduit.setPadding(new Insets(-60,0,30,30));
-          vbProduit.setSpacing(50);
-          vbProduit.setStyle("-fx-background-color:#E3F9FE;-fx-background-radius:20px;");
-         
-          vbProduit.setPrefSize(200, 150);
-          vbProduit.getChildren().add(im);
-          vbProduit.getChildren().add(t1);
-          vbProduit.getChildren().add(t);
-          i++;
-          
-          if(i%3!=1)
-          {
-            hb.getChildren().add(vbProduit) ;
-          }
-          else
-          {
-            hb = new HBox();
-            hb.setPadding(new Insets(0,0,0,0));
-            hb.setSpacing(50);
-            hb.getChildren().add(vbProduit) ;
-            vb.getChildren().add(hb); 
-           }
-                 
-      }
-        sp.setContent(vb);
-         anchorEvent.getChildren().setAll(sp);
-           
-    }
-
-   
-
+   public void EffacerRefuge(ActionEvent e) throws SQLException {
+       
+   }
 }
--- src/zanimaux/GUI/RefugeDashboardController.java
+++ src/zanimaux/GUI/RefugeDashboardController.java
@@ -5,15 +5,23 @@
  */
 package zanimaux.GUI;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -43,7 +51,37 @@ public class RefugeDashboardController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }  
+        @FXML
+     void GestRefuges(ActionEvent event) throws SQLException {
+
+        try {
+        Stage stage=(Stage) GestRef.getScene().getWindow(); 
+        stage.setTitle("Vos Refuges");
+        Parent root = FXMLLoader.load(getClass().getResource("GestionRefuges.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        } catch (IOException ex) {
+           Logger.getLogger(RefugeController.class.getName()).log(Level.SEVERE, null, ex);
+       }
+     }
+        @FXML
+     void GestAnimaux(ActionEvent event) throws SQLException {
+
+        try {
+        Stage stage=(Stage) GestAnim.getScene().getWindow(); 
+        stage.setTitle("Vos Refuges");
+        Parent root = FXMLLoader.load(getClass().getResource("GestionAnimaux.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        } catch (IOException ex) {
+           Logger.getLogger(RefugeController.class.getName()).log(Level.SEVERE, null, ex);
+       }
+        
+
+    }
 
     
 }

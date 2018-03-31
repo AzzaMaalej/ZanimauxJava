fcdf2803f7f22175a058b25bd4a8836d4e251c9a
MariamRiahi <mariam.riahi@esprit.tn>
2018-03-31 01:25:49 +0100

    hay taswira ya azza

--- src/zanimaux/Service/Userservice.java
+++ src/zanimaux/Service/Userservice.java
@@ -20,10 +20,7 @@ import java.util.logging.Logger;
 
 import javafx.scene.control.Label;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> df3d3cae9db46e94342d5a78730ba2c568e771b6
 
 /**
  *
@@ -200,16 +197,12 @@ public class Userservice {
         User listForm = new User();
         try {  
             String requete = "SELECT * FROM fos_user WHERE cin='"+i+"' ";
-<<<<<<< HEAD
+
               PreparedStatement st = con.prepareStatement(requete);
 
 
          //  PreparedStatement st=con.prepareStatement(requete);
-=======
 
-
-           PreparedStatement st=con.prepareStatement(requete);
->>>>>>> df3d3cae9db46e94342d5a78730ba2c568e771b6
 
            
 
@@ -331,9 +324,8 @@ public class Userservice {
 //        }
         
 
-<<<<<<< HEAD
+
+
 
 }
-=======
-}
->>>>>>> df3d3cae9db46e94342d5a78730ba2c568e771b6
+

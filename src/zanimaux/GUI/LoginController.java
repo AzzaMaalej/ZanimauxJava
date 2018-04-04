93e6e9ebd89435438c473419978ac02480a04e8b
lims74 <belhassen.limam@gmail.com>
2018-04-04 09:46:20 +0100

    Retablissement des pages ajout et affichager pour parc

--- src/zanimaux/GUI/LoginController.java
+++ src/zanimaux/GUI/LoginController.java
@@ -186,7 +186,7 @@ public class LoginController implements Initializable {
                     Session.setLoggedInUser(result);
                     // TODO: Proceed to other page
                     
-                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("GestionParc.fxml"));
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AccueilDresseur.fxml"));
                     Parent root = (Parent) fxmlLoader.load();
                     Stage secondStage = new Stage();
                     secondStage.setScene(new Scene(root));

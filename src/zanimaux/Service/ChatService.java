98e9ca2cbd474e3b707230ba306795596b7b6253
Azza <azza.maalej@esprit.tn>
2018-04-05 22:34:20 +0200

    quiz

---
+++ src/zanimaux/Service/ChatService.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zanimaux.Service;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import zanimaux.Technique.DataSource;
+import zanimaux.entities.Questionnaire_chat;
+
+/**
+ *
+ * @author Azza
+ */
+public class ChatService {
+    private static ChatService instance;
+    public static ChatService getInstance() throws SQLException {
+       
+         if (instance == null) {
+            instance = new ChatService();
+        }
+        return instance;//To change body of generated methods, choose Tools | Templates.
+    }
+       public Connection con = DataSource.getInstance().getCon();
+    public Statement ste;
+
+    public ChatService() throws SQLException {
+       ste = con.createStatement();
+    }
+    
+     public int AfficherIdChatRace(int a,int b,int c,int d,int e,int f) throws SQLException{
+        Questionnaire_chat chat=new Questionnaire_chat();
+        String requete = "SELECT race FROM questionnaire_chat WHERE tolererChien="+a+" AND dynamique="+b+" AND affectueux="+c+" AND chutePoils="+d+" AND intelligent="+e+" AND acceptationEtranger="+f;
+        ResultSet rs = ste.executeQuery(requete);
+         while(rs.next()){
+             chat.setRace(rs.getInt("race"));
+         }
+        
+        return chat.getRace();
+    }
+}
---
+++ src/zanimaux/Service/ChienService.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zanimaux.Service;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import zanimaux.Technique.DataSource;
+import zanimaux.entities.Questionnaire_chien;
+
+/**
+ *
+ * @author Azza
+ */
+public class ChienService {
+    private static ChienService instance;
+    public static ChienService getInstance() throws SQLException {
+       
+         if (instance == null) {
+            instance = new ChienService();
+        }
+        return instance;//To change body of generated methods, choose Tools | Templates.
+    }
+       public Connection con = DataSource.getInstance().getCon();
+    public Statement ste;
+
+    public ChienService() throws SQLException {
+       ste = con.createStatement();
+    }
+    public int AfficherIdChienRace(int a,int b,int c,int d,int e,int f) throws SQLException{
+        Questionnaire_chien chien=new Questionnaire_chien();
+        String requete = "SELECT race FROM questionnaire_chien WHERE tolererChien="+a+" AND calme="+b+" AND affectueux="+c+" AND chutePoils="+d+" AND intelligent="+e+" AND tolererChat="+f;
+        ResultSet rs = ste.executeQuery(requete);
+         while(rs.next()){
+             chien.setRace(rs.getInt("race"));
+         }
+        
+        return chien.getRace();
+    }
+}

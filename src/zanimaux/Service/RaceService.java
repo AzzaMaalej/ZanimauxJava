d7fb2251a21705743f94f343dce51ca650b963da
Azza <azza.maalej@esprit.tn>
2018-04-07 00:31:12 +0200

    quiz chat

--- src/zanimaux/Service/RaceService.java
+++ src/zanimaux/Service/RaceService.java
@@ -6,9 +6,14 @@
 package zanimaux.Service;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import zanimaux.Technique.DataSource;
+import zanimaux.entities.Race;
+import zanimaux.entities.Refuge;
 
 /**
  *
@@ -28,5 +33,30 @@ public class RaceService {
 
     public RaceService() throws SQLException {
        ste = con.createStatement();
+    }
+     public Race RechercherRaceById(int i)
+    { 
+
+        Race listForm = new Race();
+        try {  
+            String requete = "SELECT * FROM race WHERE id="+i+"";
+
+           
+            ResultSet rs = ste.executeQuery(requete);
+
+             while(rs.next()){
+                 listForm.setId(rs.getInt("id"));
+                 listForm.setInformations(rs.getString("informations"));
+                 listForm.setRace(rs.getString("race"));
+                 listForm.setType(rs.getString("type"));
+                 listForm.setPhoto(rs.getString("photo"));
+                 
+            }
+             
+        } catch (SQLException ex) {
+            Logger.getLogger(Articleservice.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return listForm;
+    
     }
 }

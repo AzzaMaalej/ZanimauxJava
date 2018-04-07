bf478e06105974fb12ccc027071ca70b967d41c7
lims74 <belhassen.limam@gmail.com>
2018-04-07 16:29:11 +0100

    Api Avis fonctionnelle

--- src/zanimaux/GUI/AccueilDresseurController.java
+++ src/zanimaux/GUI/AccueilDresseurController.java
@@ -13,6 +13,7 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -35,7 +36,9 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.controlsfx.control.Rating;
+import zanimaux.Service.AvisService;
 import zanimaux.Service.ParcService;
+import zanimaux.entities.Avis;
 import zanimaux.entities.Parc;
 import zanimaux.entities.User;
 import zanimaux.util.Session;
@@ -69,91 +72,166 @@ public class AccueilDresseurController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-     ParcService m=null;
+        ParcService m=null;
         try {
             m = new ParcService();
         } catch (SQLException ex) {
             Logger.getLogger(ParcController.class.getName()).log(Level.SEVERE, null, ex);
         }
         User user=Session.getLoggedInUser();
-            String cin=user.getCin();
+        String cin=user.getCin();
         ResultSet r =m.AfficherParcByCin(cin);
         Parc m1=new Parc();
         r= m.AfficherParcByCin(cin);
         ScrollPane sp = new ScrollPane();
-    
         sp.setPrefSize(900, 650);
-//         sp.setMaxSize(Control.USE_COMPUTED_SIZE, Control.USE_COMPUTED_SIZE);
+        //         sp.setMaxSize(Control.USE_COMPUTED_SIZE, Control.USE_COMPUTED_SIZE);
 //         sp.setMinSize(Control.USE_COMPUTED_SIZE, Control.USE_COMPUTED_SIZE);
-        VBox vb = new VBox();
-        HBox hb =null;
-        vb.setPadding(new Insets(100, 30, 0, 30));
-        vb.setSpacing(100);
-        int i=0;
-         try{
-      while(r.next())
-      { 
-          m1.setId(r.getString("id"));
-          m1.setNomParc(r.getString("nomParc"));
-          m1.setCategorieDressage(r.getString("CategorieDressage"));
-          m1.setAdresseParc(r.getString("adresseParc"));
-          m1.setVilleParc(r.getString("villeParc"));
-          m1.setCodePostaleParc(r.getInt("codePostaleParc"));
-          m1.setPhotoParc(r.getString("photoParc"));
-          m1.setCinDresseur(r.getString("cinDresseur"));
-          ImageView im= new ImageView();
-          Image image= new Image("zanimaux/ImageUtile/"+m1.getPhotoParc(),150,120,false,false) ;
-          im.setImage(image);
-          Text t1 =new Text(m1.getNomParc());
-          t1.setFont(Font.font("Verdana", 16));
-          Text t3 = new Text("Dressage de : ");
-          t3.setFont(Font.font("Verdana", 15));
-          t3.setFill(Color.web("#0076a3"));
-          Text t2 = new Text(m1.getCategorieDressage());
-          t2.setFont(Font.font("Verdana", 14));
-          Text t4 = new Text("Adresse : ");
-          t4.setFont(Font.font("Verdana", 15));
-          t4.setFill(Color.web("#0076a3"));
-          Text t =new Text(m1.getAdresseParc()+" "+m1.getVilleParc()+", "+m1.getCodePostaleParc());
-          t.setFont(Font.font("Verdana", 14) );
-         Rating rating = new Rating(5);  
-         
-         
-          VBox vbParc = new VBox(); 
-          vbParc.setPadding(new Insets(-60,0,30,30));
-          vbParc.setSpacing(20);
-          vbParc.setStyle("-fx-background-color:#E3F9FE;-fx-background-radius:20px;");
-          vbParc.setPrefSize(200, 150);
-          vbParc.getChildren().add(im);
-          vbParc.getChildren().add(t1);
-          vbParc.getChildren().add(t3);
-          vbParc.getChildren().add(t2);
-          vbParc.getChildren().add(t4);
-          vbParc.getChildren().add(t);
-          vbParc.getChildren().add(rating);
-          
-          i++;
-          System.out.println(m1.getId()+" "+m1.getPhotoParc());
-          if(i%3!=1)
-          {
-            hb.getChildren().add(vbParc) ;
-            
-          }
-          else
-          {
-            hb = new HBox();
-            hb.setPadding(new Insets(0,0,0,0));
-            hb.setSpacing(50);
-            hb.getChildren().add(vbParc) ;
-            vb.getChildren().add(hb); 
-           }
-                 
-      }
-      }catch( SQLException e){}
-        sp.setContent(vb);
-               
+VBox vb = new VBox();
+HBox hb =null;
+vb.setPadding(new Insets(100, 30, 0, 30));
+vb.setSpacing(100);
+int i=0;
+try{
+    while(r.next())
+    {
+        m1.setId(r.getString("id"));
+        m1.setNomParc(r.getString("nomParc"));
+        m1.setCategorieDressage(r.getString("CategorieDressage"));
+        m1.setAdresseParc(r.getString("adresseParc"));
+        m1.setVilleParc(r.getString("villeParc"));
+        m1.setCodePostaleParc(r.getInt("codePostaleParc"));
+        m1.setPhotoParc(r.getString("photoParc"));
+        m1.setCinDresseur(r.getString("cinDresseur"));
+        ImageView im= new ImageView();
+        Image image= new Image("zanimaux/ImageUtile/"+m1.getPhotoParc(),150,120,false,false) ;
+        im.setImage(image);
+        Text t1 =new Text(m1.getNomParc());
+        t1.setFont(Font.font("Verdana", 16));
+        Text t3 = new Text("Dressage de : ");
+        t3.setFont(Font.font("Verdana", 15));
+        t3.setFill(Color.web("#0076a3"));
+        Text t2 = new Text(m1.getCategorieDressage());
+        t2.setFont(Font.font("Verdana", 14));
+        Text t4 = new Text("Adresse : ");
+        t4.setFont(Font.font("Verdana", 15));
+        t4.setFill(Color.web("#0076a3"));
+        Text t =new Text(m1.getAdresseParc()+" "+m1.getVilleParc()+", "+m1.getCodePostaleParc());
+        t.setFont(Font.font("Verdana", 14) );
         
-        anchorEvent.getChildren().setAll(sp);
+        AvisService av = new AvisService();
+        Avis av1= new Avis();
+        
+        boolean r1 = av.VerifierAvis(m1.getId(), cin);
+        
+        if (r1==false){
+            Rating rating = new Rating (5);
+            rating.setRating(0);
+            Button note = new Button();
+            note.setId(String.valueOf(m1.getId()));
+            note.setOnAction((ActionEvent event) -> {
+                try {
+                    AvisService av2 = new AvisService();
+                    User user1 = Session.getLoggedInUser();
+                    String cin1 = user1.getCin();
+                    String e1 =((Node)event.getSource()).getId();
+                    
+                    Avis a = new Avis(e1, rating.getRating(), cin1);
+                    av2.ajouterAvis(a);
+                    System.out.println("Avis Modifié");
+                    
+                }catch (SQLException ex) {
+                    Logger.getLogger(AccueilDresseurController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+            VBox vbParc = new VBox();
+            vbParc.setPadding(new Insets(-60,0,30,30));
+            vbParc.setSpacing(20);
+            vbParc.setStyle("-fx-background-color:#E3F9FE;-fx-background-radius:20px;");
+            vbParc.setPrefSize(200, 150);
+            vbParc.getChildren().add(im);
+            vbParc.getChildren().add(t1);
+            vbParc.getChildren().add(t3);
+            vbParc.getChildren().add(t2);
+            vbParc.getChildren().add(t4);
+            vbParc.getChildren().add(t);
+            vbParc.getChildren().add(rating);
+            vbParc.getChildren().add(note);
+            
+            
+            
+            i++;
+            System.out.println(m1.getId()+" "+m1.getPhotoParc());
+            
+            if(i%3!=1)
+            {
+                hb.getChildren().add(vbParc) ;
+                
+            }
+            else
+            {
+                hb = new HBox();
+                hb.setPadding(new Insets(0,0,0,0));
+                hb.setSpacing(50);
+                hb.getChildren().add(vbParc) ;
+                vb.getChildren().add(hb);
+            }
+            
+            
+        }
+        else{
+            ResultSet r2=av.AfficherAvis(m1.getId(), cin);
+            while(r2.next()){
+                av1.setAvis(r2.getDouble("avis"));
+                
+            }
+            Text rat = new Text(Double.toString(av1.getAvis()));
+            VBox vbParc = new VBox();
+            vbParc.setPadding(new Insets(-60,0,30,30));
+            vbParc.setSpacing(20);
+            vbParc.setStyle("-fx-background-color:#E3F9FE;-fx-background-radius:20px;");
+            vbParc.setPrefSize(200, 150);
+            vbParc.getChildren().add(im);
+            vbParc.getChildren().add(t1);
+            vbParc.getChildren().add(t3);
+            vbParc.getChildren().add(t2);
+            vbParc.getChildren().add(t4);
+            vbParc.getChildren().add(t);
+            vbParc.getChildren().add(rat);
+            
+            
+            
+            
+            i++;
+            System.out.println(m1.getId()+" "+m1.getPhotoParc());
+            
+            if(i%3!=1)
+            {
+                hb.getChildren().add(vbParc) ;
+                
+            }
+            else
+            {
+                hb = new HBox();
+                hb.setPadding(new Insets(0,0,0,0));
+                hb.setSpacing(50);
+                hb.getChildren().add(vbParc) ;
+                vb.getChildren().add(hb);
+            }
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+}       catch( SQLException e){}
+sp.setContent(vb);
+anchorEvent.getChildren().setAll(sp);
         
     }     
     
@@ -212,5 +290,7 @@ public class AccueilDresseurController implements Initializable {
     @FXML
     private void connexionAction(ActionEvent event) {
     }
+    
+   
        
 }
---
+++ src/zanimaux/Service/AvisService.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zanimaux.Service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import zanimaux.Technique.DataSource;
+import zanimaux.entities.Avis;
+import zanimaux.entities.User;
+import zanimaux.util.Session;
+
+/**
+ *
+ * @author Azza
+ */
+public class AvisService {
+    private static AvisService instance;
+    public static AvisService getInstance() throws SQLException {
+       
+         if (instance == null) {
+            instance = new AvisService();
+        }
+        return instance;//To change body of generated methods, choose Tools | Templates.
+    }
+     public Connection con = DataSource.getInstance().getCon();
+     public Statement ste;
+     public AvisService() throws SQLException 
+    {
+        ste=con.createStatement();
+    }
+     
+   public boolean ajouterAvis(Avis g) throws SQLException {
+       String req="INSERT INTO avis (idParc,avis,cinUser) VALUES (?,?,?)";
+        Avis a =new Avis();
+        User user=Session.getLoggedInUser();
+        Userservice us= new Userservice();
+       try {
+       
+       PreparedStatement pre= con.prepareStatement(req);
+        
+        
+        pre.setString(1, g.getIdParc());
+        pre.setDouble(2, g.getAvis());
+        pre.setString(3, user.getCin());
+        pre.executeUpdate();   
+     System.out.println("ajout reussit");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+            return false;
+    }
+   public ResultSet AfficherAvis(String i, String e)
+    { 
+        ResultSet rs=null;
+        try {  
+            String requete = "SELECT * FROM avis WHERE cinUser='"+e+"'AND idParc='"+i+"'"  ;
+
+            rs = ste.executeQuery(requete);
+            
+             }catch (SQLException ex) {
+                 System.out.println(" erreur AfficherAvis()");
+        }
+        return rs ;
+    
+    }
+   
+    public boolean VerifierAvis(String i, String e)
+    { 
+        
+        try {  
+            String requete = "SELECT * FROM avis WHERE cinUser='"+e+"'AND idParc='"+i+"'"  ;
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(requete);
+            if(rs.next()){
+                return true;
+            }
+            
+             }catch (SQLException ex) {
+                 System.out.println(" erreur VerifierAvis()");
+        }
+        return false;
+    
+    }
+   
+             
+        
+    
+    
+   
+   
+    
+  
+    
+}

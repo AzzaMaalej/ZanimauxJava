8f5921af387a4e7c29e99630417577dad65b68c1
Azza <azza.maalej@esprit.tn>
2018-04-08 21:47:11 +0200

    refuge Plus proche

--- src/zanimaux/GUI/LoginController.java
+++ src/zanimaux/GUI/LoginController.java
@@ -144,7 +144,7 @@ public class LoginController implements Initializable {
                     Session.setLoggedInUser(result);
                     // TODO: Proceed to other page
                     
-                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("RefugeDashboard.fxml"));
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("RefugePlusProche.fxml"));
                     Parent root = (Parent) fxmlLoader.load();
                     Stage secondStage = new Stage();
                     secondStage.setScene(new Scene(root));
@@ -166,7 +166,7 @@ public class LoginController implements Initializable {
 
                  // FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("afficheEvent.fxml"));
 
-                   FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("accueilOumaima.fxml"));
+                   FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Quiz.fxml"));
                   //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("afficheEvent.fxml"));
 
                   //FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ProfilManager.fxml"));          
